48d7bd3dbc2af526b6ce597e6bac38e9328f6911
JoAsia <joanna16ka@gmail.com>
2024-03-03 15:41:53 +0100

    v1

--- app/src/main/java/com/example/foodiemeetup/MainActivity.kt
+++ app/src/main/java/com/example/foodiemeetup/MainActivity.kt
@@ -5,21 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.foodiemeetup.ViewModels.LoginViewModel
 import com.example.foodiemeetup.app.FoodieMeetupApp
+import com.example.foodiemeetup.screens.MainPage
+import com.example.foodiemeetup.ui.theme.FoodieMeetUpTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var myViewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         myViewModel = LoginViewModel()
-        setContent {
+      /*  setContent {
             FoodieMeetupApp(viewModel=myViewModel)
 
-        }
- /*     setContent {
+        }*/
+      setContent {
             FoodieMeetUpTheme {
                 MainPage()
             }
-        }*/
+        }
     }
 }
 
