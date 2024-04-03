30d5e54c18714e3f060e724fa9edfbd0d41258a0
pstawicki <stawpaw79750@gmail.com>
2024-04-03 19:49:29 +0200

    Home screen adding map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,5 @@ dependencies {
     implementation ("androidx.datastore:datastore-preferences:1.0.0")
 
     implementation ("androidx.navigation:navigation-compose:2.6.0")
+    implementation ("org.osmdroid:osmdroid-android:6.1.18")
 }
