e79947efa6f6c2252363d746a7b3c9296760a06a
JoAsia <joanna16ka@gmail.com>
2024-04-19 20:27:56 +0200

    map marker dialog

--- app/src/main/java/com/example/foodiemeetup/screens/HomeScreen.kt
+++ app/src/main/java/com/example/foodiemeetup/screens/HomeScreen.kt
@@ -1,12 +1,9 @@
 package com.example.foodiemeetup.screens
 
-import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -17,12 +14,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,23 +23,24 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 import com.example.foodiemeetup.R
 import com.example.foodiemeetup.ViewModels.HomeScreenViewModel
 import com.example.foodiemeetup.models.MapPointsResponseModel
-import com.example.foodiemeetup.models.UserResponseModel
 import com.example.foodiemeetup.ui.theme.BgColor
 import com.example.foodiemeetup.ui.theme.Primary
 import com.example.foodiemeetup.ui.theme.Secondary
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker.OnMarkerClickListener
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel) {
@@ -77,10 +69,10 @@ fun HomeScreen(viewModel: HomeScreenViewModel) {
             // Wyświetl mapę, gdy dane zostały pobrane
             OpenStreetMap(
                 center = GeoPoint(53.0138, 18.5984), // Współrzędne dla Torunia
-                points = points
+                points = points,
+                viewModell = viewModel
             )
         }
-        //viewModel.onPointClick() //Okno po kliknięciu w pinezkę - szkic
     }
 
     if(viewModel.isDialogShown){
@@ -90,7 +82,9 @@ fun HomeScreen(viewModel: HomeScreenViewModel) {
             },
             onConfirm = {
                 //viewmodel.dsd
-            }
+            },
+            viewModel.pointName,
+            viewModel.pointAddress
         )
     }
 }
@@ -98,7 +92,8 @@ fun HomeScreen(viewModel: HomeScreenViewModel) {
 @Composable
 fun OpenStreetMap(
     center: GeoPoint,
-    points: List<MapPointsResponseModel>
+    points: List<MapPointsResponseModel>,
+    viewModell: HomeScreenViewModel
 ) {
     AndroidView(
         factory = { context ->
@@ -113,10 +108,18 @@ fun OpenStreetMap(
                     position = GeoPoint(point.lat, point.lon)
                     title = point.name
                     snippet = point.address
-                    //setIcon(ContextCompat.getDrawable(context, R.drawable.marker_icon)) dodawanie wlasnej ikony
+                    setIcon(ContextCompat.getDrawable(context, R.drawable.map_marker))  //dodawanie wlasnej ikony
+
                 }
                 mapView.overlays.add(marker)
+                marker.setOnMarkerClickListener(OnMarkerClickListener { marker, mapView ->
+                    marker.showInfoWindow()
+                    mapView.controller.animateTo(marker.position)
+                    viewModell.onPointClick(point.name, point.address)
+                    true
+                })
             }
+
 
             mapView
         }
@@ -124,7 +127,7 @@ fun OpenStreetMap(
 }
 
 @Composable
-fun ChosenPlaceDialog( onDismiss:() -> Unit, onConfirm:() -> Unit){
+fun ChosenPlaceDialog( onDismiss:() -> Unit, onConfirm:() -> Unit, pointName: String, pointAddress: String){
     Dialog(
         onDismissRequest = {
             onDismiss()
@@ -145,19 +148,20 @@ fun ChosenPlaceDialog( onDismiss:() -> Unit, onConfirm:() -> Unit){
 
             ) {
             Text(
-                text = "Restauracja X",
+                text = pointName,
                 textAlign = TextAlign.Center,
                 style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Normal, fontStyle = FontStyle.Normal ),
                 color = colorResource(id = R.color.colorText),
                 modifier = Modifier.padding(30.dp, 30.dp, 30.dp, 0.dp)
             )
             Text(
-                text = "Address",
+                text = pointAddress,
                 textAlign = TextAlign.Center,
                 style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Normal, fontStyle = FontStyle.Normal ),
                 color = colorResource(id = R.color.colorText),
                 modifier = Modifier.padding(30.dp, 5.dp)
             )
+
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
